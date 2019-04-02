9cafa61a3edad5ad84f8ede415ef2488ac881ce9
Aaron Burmeister <aaron.burmeister@ils-gmbh.net>
2019-04-02 17:40:21 +0200

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+# @civet/events [![npm version](https://badge.fury.io/js/%40civet%2Fevents.svg)](http://npmjs.com/package/%40civet%2Fevents)
+
+This is a Civet module.
+Please visit [civet.hatsu.moe](http://civet.hatsu.moe) for more information.
---
+++ src/EventReceiver.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+
+class EventReceiver {
+  subscribe(resource, options, handler) {
+    if (typeof handler !== 'function') throw new Error('Handler must be a function');
+    const unsubscribe = this.handleSubscribe(resource, options, handler);
+    if (typeof unsubscribe !== 'function') {
+      // eslint-disable-next-line no-console
+      console.warn(
+        'EventReceiver.handleSubscribe should return a callback to cancel the subscription. Ignoring this warning may result in the execution of obsolete handlers and potential memory leaks.',
+      );
+    }
+    return unsubscribe;
+  }
+}
+
+const isEventReceiver = eventReceiver => eventReceiver instanceof EventReceiver;
+
+const eventReceiverPropType = PropTypes.instanceOf(EventReceiver);
+
+export default EventReceiver;
+export { isEventReceiver, eventReceiverPropType };
