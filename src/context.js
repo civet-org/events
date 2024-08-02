1e741d628172a16d450d7ea14103ab555cf66c04
Aaron <4709780+2manyvcos@users.noreply.github.com>
2024-08-02 16:07:38 +0200

    Closes #4

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
 
 export const ConfigContext = React.createContext({});
+ConfigContext.displayName = 'Events.ConfigContext';
 export const useConfigContext = () => useContext(ConfigContext);
